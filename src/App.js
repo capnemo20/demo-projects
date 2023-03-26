e120eac7396b7b0d826c97fc72d3d9e2b064051d
capnemo20 <z.sujon20@outlook.com>
2023-03-26 09:23:13 +0100

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Counter from './components/Counter/Counter';
+import Navbar from './components/Navbar/Navbar';
 import Test from './components/Test/Test';
 import Test2 from './components/Test2/Test2';
 
@@ -15,15 +16,7 @@ function App() {
   
   return (
     <div className="App">
-      {/* <Test></Test>
-      <Test2></Test2> */}
-      <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <Counter
-        count = {count}
-        handleAdd = {handleAdd}
-        handleMinus = {handleMinus}
-      ></Counter>
+    <Navbar></Navbar>
 
     </div>
   );
