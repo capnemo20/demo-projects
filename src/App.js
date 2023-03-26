bc05a2b27baed380bfa0c495a57edcf907e71f05
capnemo20 <z.sujon20@outlook.com>
2023-03-26 06:42:12 +0100

    passing data through components

--- src/App.js
+++ src/App.js
@@ -4,12 +4,27 @@ import Test from './components/Test/Test';
 import Test2 from './components/Test2/Test2';
 
 function App() {
+  const myObj = {
+    name:"jeff",
+    author: "kinney"
+  }
   return (
     <div className="App">
      <Test name = "kiddo"></Test>
      <Counter></Counter>
+     <Greet name = {myObj.name} author = {myObj.author}></Greet>
     </div>
   );
+}
+
+function Greet(props){
+    console.log(props);
+    const {name, author} = props;
+    return(
+      <div>
+        <h1>{`my name is ${name} author name is ${author}`}</h1>
+      </div>
+    )
 }
 
 export default App;
