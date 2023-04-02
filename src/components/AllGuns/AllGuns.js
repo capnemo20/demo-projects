7f0c3cbaf1a56f04794c6c3b4332690d9cc9d6e9
capnemo20 <z.sujon20@outlook.com>
2023-04-02 13:40:11 +0100

    card added#

--- src/components/AllGuns/AllGuns.js
+++ src/components/AllGuns/AllGuns.js
@@ -1,23 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import "./AllGuns.css"
+import SingleGun from '../SingleGun/SingleGun';
+import "./AllGuns.css";
 
 const AllGuns = () => {
     const [guns, setGuns] = useState([]);
     console.log(guns);
-    const datas = async()=>{
-        const res = await fetch(`https://raw.githubusercontent.com/mir-hussain/guns/main/data.json`);
-        const data =await res.json();
-        setGuns(data);
-    }
+    // const datas = async()=>{
+    //     const res = await fetch(`https://raw.githubusercontent.com/mir-hussain/guns/main/data.json`);
+    //     const data =await res.json();
+    //     setGuns(data);
+    // }
     useEffect(()=>{
-        // fetch(`https://raw.githubusercontent.com/mir-hussain/guns/main/data.json`)
-        // .then(res=>res.json())
-        // .then(data=>setGuns(data))
-        datas();
+        fetch(`https://raw.githubusercontent.com/mir-hussain/guns/main/data.json`)
+        .then(res=>res.json())
+        .then(data=>setGuns(data))
+        // datas();
     },[])
     return (
         <div>
-            <h2>all guns</h2>
+            <h1 className="text-4xl text-center font-bold mt-4" >Welcome to Kopa Samsu store</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perspiciatis error corrupti consequuntur aperiam dolores dolor et praesentium officia, molestiae dolorum consequatur placeat numquam obcaecati architecto, cumque facere maxime sequi.
+
+            </p>
+            {
+                guns.map((gun)=><SingleGun gun ={gun} />)
+            }
         </div>
     );
 };
